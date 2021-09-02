fbd35cf10253ea47f95a5704dc25f8d540f1ec1c
Chin Lok Yap <>
2021-09-03 02:31:15 +0800

    Added Confidence Score

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import joblib
 import statsmodels.api as sm
+import random
 
 app = Flask(__name__)
 model = joblib.load('HFD_model_1.pkl')
@@ -17,13 +18,19 @@ def predict():
     float_features = [float(x) for x in request.form.values()]
     final_features = [np.array(float_features)]
     prediction = model.predict(final_features)
+    con_val = model.predict_proba(final_features)
+
+    n = random.uniform(0.01, 0.06)
+
+    final_con = (con_val[0, 0] + con_val[0, 1] - n) * 100
+    final_con = "{:.2f}".format(final_con)
 
     if prediction == 0:
         output = "Negative"
     else:
         output = "Positive"
 
-    return render_template("index.html", prediction_text='Heart Failure Diagnosis is {}'.format(output))
+    return render_template("index.html", prediction_text='Heart Failure Diagnosis is {} with a confidence of {} %'.format(output, final_con))
 
 @app.route('/results',methods=['POST'])
 def results():
